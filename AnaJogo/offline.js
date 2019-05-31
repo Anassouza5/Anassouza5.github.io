b7c73068187cf1b6f0dee85ed92b689fba0caad5
Anassouza5 <50372598+Anassouza5@users.noreply.github.com>
2019-05-31 14:36:28 -0300

    Add files via upload

---
+++ AnaJogo/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1559324065,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pistoladoida-sheet0.png",
+		"images/botãodisparar-sheet0.png",
+		"images/balacontro-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setadireita-sheet0.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setabaixo-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/baloes-sheet0.png",
+		"images/botãodestruidor-sheet0.png",
+		"images/barradevida-sheet0.png",
+		"images/powerup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
